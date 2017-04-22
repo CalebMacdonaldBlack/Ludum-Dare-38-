8c60ecfcb9edbbe98179a38420943628ee6e03c5
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-04-22 21:47:50 +1000

    score

--- Scripts/Bomb.cs
+++ Scripts/Bomb.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Bomb : MonoBehaviour {
 
 	public GameObject Target;
--- Scripts/Bullet.cs
+++ Scripts/Bullet.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.UI;
+
 public class Bullet : MonoBehaviour {
-
-
+	public static int score = 0;
 	// Use this for initialization
 	void Start () {
 	}
@@ -20,6 +21,14 @@ public class Bullet : MonoBehaviour {
 		if (collision.collider.tag == "Tile" || collision.collider.tag == "Bomb") {
 			Destroy (collision.collider.gameObject);
 			Destroy (this.gameObject);
+			if (collision.collider.tag == "Bomb") {
+				score += 100;
+			}
 		}
+	}
+
+	void OnGUI(){
+		GUI.Label (new Rect(20,20,100,100), "Score: " + score);
+		//Alternatively use
 	}
 }
