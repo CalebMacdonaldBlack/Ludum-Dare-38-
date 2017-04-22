a29f57bade0eb93bae3d6dfff675c99f494d4f3f
LSchaumburg <luke.schaumburg@my.jcu.edu.au>
2017-04-22 17:59:19 +1000

    created bugs

---
+++ Scripts/bugBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bugBehaviour : MonoBehaviour {
+
+	public Transform Target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 vectorToTarget = Target.position - transform.position;
+		float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+		Quaternion q = Quaternion.AngleAxis(angle + 90, Vector3.forward);
+		transform.rotation = q;
+	}
+}
