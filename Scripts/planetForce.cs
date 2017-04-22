e5f6a9f806e3ea43f9dce2b8a64658fe9adcf17d
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-04-22 15:48:09 +1000

    fuck

--- Scripts/planetForce.cs
+++ Scripts/planetForce.cs
@@ -9,13 +9,11 @@ public class planetForce : MonoBehaviour {
 		public void FixedUpdate() {
 		Debug.Log (pullRadius);
 		foreach (Collider2D collider in Physics2D.OverlapCircleAll(transform.position, pullRadius)) {
-			Debug.Log ("Finding Objects");
-			// calculate direction from target to me
-			Vector2 forceDirection = transform.position - collider.transform.position;
-
-			Debug.Log ("Pulling Objects");
-			// apply force on target towards me
-			collider.GetComponent<Rigidbody2D>().AddForce(forceDirection.normalized * pullForce * Time.fixedDeltaTime);
+			
+			if (collider.tag != "Tile") {
+				Vector2 forceDirection = transform.position - collider.transform.position;
+				collider.GetComponent<Rigidbody2D>().AddForce(forceDirection.normalized * pullForce * Time.fixedDeltaTime);
+			}
 		}
 	}
 }
