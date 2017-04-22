71eefa09ea8b54294dba8ad482306272683343c0
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-04-22 15:00:38 +1000

    init commit

---
+++ Game.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+	public GameObject tile;
+	public int radius;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("GAME STARTED BITCHES");
+
+		int searchArea = radius * 3;
+		for (int x = 0; x < searchArea; x++) {
+			for (int y = 0; y < searchArea; y++) {
+				if (((x - 0) * (x - 0) + (y - 0) * (y - 0)) <= radius * radius) {
+					Instantiate(tile, new Vector3(x, y, 1), tile.transform.rotation);
+					Instantiate(tile, new Vector3(x, y - y - y, 1), tile.transform.rotation);
+					Instantiate(tile, new Vector3(x - x - x, y, 1), tile.transform.rotation);
+					Instantiate(tile, new Vector3(x - x - x, y - y - y, 1), tile.transform.rotation);
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
