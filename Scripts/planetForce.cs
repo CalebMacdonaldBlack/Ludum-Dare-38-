9862a419686ff2a2ee544b6e7b4f7e49338c4530
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-04-23 15:06:08 +1000

    fixedd bugs

--- Scripts/planetForce.cs
+++ Scripts/planetForce.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class planetForce : MonoBehaviour {
 		public float pullRadius = 5000;
 		public float pullForce = 10;
+		public bool isCircle;
 
 		public void FixedUpdate() {
-		foreach (Collider2D collider in Physics2D.OverlapCircleAll(transform.position, pullRadius)) {
-			Vector2 forceDirection = transform.position - collider.transform.position;
+		var pos = transform.position;
+		if (isCircle) {
+			pos = GetComponent<CircleCollider2D> ().transform.position;
+		}
+		var all = Physics2D.OverlapCircleAll (pos, pullRadius);
+		foreach (Collider2D collider in all) {
+			Vector2 forceDirection = pos - collider.transform.position;
 			switch (collider.tag) {
 			case "Bomb":
 				collider.GetComponent<Rigidbody2D> ().AddForce (forceDirection.normalized * pullForce / 40 * Time.fixedDeltaTime);
 				break;
 
 			case "Player":
-				collider.GetComponent<Rigidbody2D> ().AddForce (forceDirection.normalized * pullForce * Time.fixedDeltaTime);
+				if(collider.GetContacts(all) <= 0){
+					collider.GetComponent<Rigidbody2D> ().AddForce (forceDirection.normalized * pullForce * Time.fixedDeltaTime);
+				}
 				break;
 			}
 		}
